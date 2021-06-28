c500c0f700ce33e1b690fe8d5989529a6a6487c7
hwanse <khsb2012@gmail.com>
2021-06-28 20:25:12 +0900

    feat:  검색결과 구현 2파트 실습

--- 1-vanilla/js/views/ResultView.js
+++ 1-vanilla/js/views/ResultView.js
@@ -4,17 +4,32 @@ const tag = '[ResultView]'
 
 const ResultView = Object.create(View)
 
+ResultView.messages = {
+    NO_RESULT : '검색 결과가 없습니다'
+}
+
 ResultView.setup = function (el) {
     this.init(el)
 }
 
 ResultView.render = function (data = []) {
     console.log(tag, 'render()', data)
-    this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : '검색 결과가 없습니다'
+    this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : this.messages.NO_RESULT
 }
 
 ResultView.getSearchResultsHtml = function (data) {
-    debugger
+    return data.reduce((html, item) => {
+        html += this.getSearchItemHtml(item)
+        return html
+    }, '<ul>') + '</ul>'
+}
+
+ResultView.getSearchItemHtml = function (item) {
+    // es6 template literal 사용 시 싱글 쿼트(')가 아니라 노멀 쿼트(숫자 1버튼 왼쪽 키)를 사용해야 한다.
+    return `<li>
+        <img src="${item.image}">
+        <p>${item.name}</p>
+    </li>`
 }
 
 export default ResultView
