9fe16fd90098436bf0e25331d03435dad18e978e
hwanse <khsb2012@gmail.com>
2021-06-29 21:13:09 +0900

    feat : 검색어 리스트 component 실습 2파트

--- 3-component/js/components/FormComponent.js
+++ 3-component/js/components/FormComponent.js
@@ -6,6 +6,11 @@ export default {
             inputValue: this.value
         }
     },
+    watch: { // 특정 뷰 모델의 변화를 감시하는 기능
+        value(newVal, oldVal) { // 함수명은 감시할 속성값과 동일하게 맞춰준다
+            this.inputValue = newVal
+        }
+    },
     methods: {
         onSubmit() {
             this.$emit('@submit', this.inputValue.trim())
--- 3-component/js/components/ListComponent.js
+++ 3-component/js/components/ListComponent.js
@@ -1,6 +1,14 @@
 export default {
     template: '#list',
     props: ['data', 'type'],
+    computed: { // vue 템플릿에서 일종의 함수처럼 쓰이는 부분
+        keywordType() {
+            return this.type === 'keyword'
+        },
+        historyType() {
+            return this.type === 'history'
+        }
+    },
     methods: {
         onClickList(keyword) {
             this.$emit('@click', keyword)
