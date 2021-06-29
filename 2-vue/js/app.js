adbbe4d5bd01bb6275241371be3a7cf5355ad92a
hwanse <khsb2012@gmail.com>
2021-06-29 12:45:01 +0900

    feat : 탭 구현 vue 실습 1파트

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -5,7 +5,11 @@ new Vue({
     data: {
         query: '',
         submitted: false,
+        tabs: ['추천 검색어', '최근 검색어'],
         searchResult: []
+    },
+    created() { // vue 인스턴스 라이프 사이클의 생성에 관한 이벤트 처리
+        this.selectedTab = this.tabs[0]
     },
     methods: {
         onSubmit(e) {
